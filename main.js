53b7e0154f56bd03e059db04e0f7b3b698948e05
AyaanSharma-GITHUB <77273072+AyaanSharma-GITHUB@users.noreply.github.com>
2021-05-29 09:21:33 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function YOLO() {
+
+    alert("hi I am ayaan");
+    
+    user_name = document.getElementById("user_name").value;
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "kwitter.html";
+
+}
